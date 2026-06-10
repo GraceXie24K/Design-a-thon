05152c906d9ea7fc62d96fc694e3290b6a6702fc
Grace Xie <gracexie@berkeley.edu>
2026-06-09 17:27:18 -0700

    fixed link

--- process.py
+++ process.py
@@ -8,7 +8,7 @@ import json
 
 
 
-url = "https://docs.google.com/spreadsheets/d/1h7Em7IrvPW-MqvnK3sl4wCrs4U98mpBP3LgxSTn2JME/export?format=csv&gid=1876965205"
+url = "https://docs.google.com/spreadsheets/d/1h7Em7IrvPW-MqvnK3sl4wCrs4U98mpBP3LgxSTn2JME/export?format=csv&gid=371215680"
 df = pd.read_csv(url)
 
 event_df = df[df["What do you wish to submit a tracking for?"] == "Event Participation"].iloc[:,1:6]
