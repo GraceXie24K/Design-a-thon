500192732145a3fda3e370312e240323b08f742f
Grace Xie <gracexie@berkeley.edu>
2026-07-22 23:18:48 -0700

    added bottom bar

--- home/script.js
+++ home/script.js
@@ -1,16 +1,6 @@
-const button = document.getElementById("menu-button");
-const links = document.querySelector("#last");
+const menuButton = document.getElementById("menu-button");
+const menu = document.getElementById("menu");
 
-button.onclick = () => {
-    if (links.style.display === "flex") {
-        links.style.display = "none";
-    } else {
-        links.style.display = "flex";
-        links.style.flexDirection = "column";
-        links.style.position = "absolute";
-        links.style.top = "60px";
-        links.style.right = "0";
-        links.style.background = "#3E3E4B";
-        links.style.width = "200px";
-    }
-};
+menuButton.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
